13474e082b0779d44be0b8b4b3079a33a82455d9
estefape <93286782+estefape@users.noreply.github.com>
2024-06-24 23:48:30 -0300

    Implementacion de Testimonios e historial de turnos (#28)
    * finalizado
    
    * Correccion de nombres de paginas y titulos
    
    ---------
    
    Co-authored-by: Maximo1201 <81261438+Maximo1201@users.noreply.github.com>

--- src/pages/dashboard/historial-de-turnos.tsx
+++ src/pages/dashboard/historial-de-turnos.tsx
@@ -1,56 +1,27 @@
-// /pages/historial-de-turnos.js
-import useTokenValidation from '@/hooks/useTokenValidation';
-import DashboardLayout from '@/layouts/DashboardLayout';
-import styles from '@/styles/HistorialDeTurnos.module.css';
-import AppFooter from '@/views/AppFooter';
+import TableBooking from "@/components/TableBooking"
+import useTokenValidation from "@/hooks/useTokenValidation";
+import DashboardLayout from "@/layouts/DashboardLayout"
+import { Box, Typography } from "@mui/material";
 
-const HistorialDeTurnos = () => {
+const HistorialCompras = () => {
 
   useTokenValidation();
-
-  const turnos = [
-    { id: 1, fecha: '2023-06-01', cliente: 'Cliente 1' },
-    { id: 2, fecha: '2023-06-02', cliente: 'Cliente 2' },
-    { id: 3, fecha: '2023-06-03', cliente: 'Cliente 3' },
-  ];
-
+  
   return (
     <DashboardLayout>
-     <div >
-     <div className={styles.tabs}>
-        <button className={styles.tabButton}>Agendar Turno</button>
-        <button className={`${styles.tabButton} ${styles.active}`}>Ver historial</button>
+      <Typography sx={{ 
+        fontSize: "22px", 
+        textAlign: "center", 
+        textTransform: "uppercase", 
+        fontWeight: "800",
+        my: "20px" }}>
+          Historial de Turnos
+      </Typography>
+      <div style={{ margin: "2rem" }}>      
+        <TableBooking />
       </div>
-      <div className={styles.tableContainer}>
-        {turnos.length > 0 ? (
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th className={styles.th}>Turno ID</th>
-                <th className={styles.th}>Fecha</th>
-                <th className={styles.th}>Cliente</th>
-              </tr>
-            </thead>
-            <tbody>
-              {turnos.map((turno) => (
-                <tr key={turno.id}>
-                  <td className={styles.td}>{turno.id}</td>
-                  <td className={styles.td}>{turno.fecha}</td>
-                  <td className={styles.td}>{turno.cliente}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>No users for this project yet</p>
-        )}
-      </div>
-     </div>
-      <footer style={{ width: "100vw" }}>
-        <AppFooter />
-      </footer>
     </DashboardLayout>
-  );
-};
+  )
+}
 
-export default HistorialDeTurnos;
+export default HistorialCompras
