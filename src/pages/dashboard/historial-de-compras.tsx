38b2468ee380af58c9f93cf6a7788e1f862985e2
Estefania Mingochea <estefape06@gmail.com>
2024-06-24 23:31:37 -0300

    Redirect a login en historial de compras al expirar token

--- src/pages/dashboard/historial-de-compras.tsx
+++ src/pages/dashboard/historial-de-compras.tsx
@@ -1,16 +1,17 @@
 import TableBooking from "@/components/TableBooking"
+import useTokenValidation from "@/hooks/useTokenValidation";
 import DashboardLayout from "@/layouts/DashboardLayout"
 
 const HistorialCompras = () => {
+
+  useTokenValidation();
+  
   return (
     <DashboardLayout>
-      <div style={{ margin: "2rem" }}>
-        
-    <TableBooking />
+      <div style={{ margin: "2rem" }}>      
+        <TableBooking />
       </div>
-
     </DashboardLayout>
-
   )
 }
 
