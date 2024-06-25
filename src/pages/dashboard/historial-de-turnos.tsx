30dc4f14c7b3f1e33a0d11c0369beea9e7597df2
Estefania Mingochea <estefape06@gmail.com>
2024-06-24 23:47:56 -0300

    Correccion de nombres de paginas y titulos

--- src/pages/dashboard/historial-de-turnos.tsx
+++ src/pages/dashboard/historial-de-turnos.tsx
@@ -1,48 +1,27 @@
-// /src/pages/dashboard/historial-de-compras.tsx
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import DashboardLayout from '@/layouts/DashboardLayout';
-import styles from '../../styles/HistorialDeTurnos.module.css';
+import TableBooking from "@/components/TableBooking"
+import useTokenValidation from "@/hooks/useTokenValidation";
+import DashboardLayout from "@/layouts/DashboardLayout"
+import { Box, Typography } from "@mui/material";
 
-const columns: GridColDef[] = [
-  { field: 'fechaCompra', headerName: 'Fecha de Compra', width: 150 },
-  { field: 'nombrePack', headerName: 'Nombre Pack', width: 150 },
-  { field: 'metodoPago', headerName: 'Método de Pago', width: 200 },
-  { field: 'monto', headerName: 'Monto', width: 100 },
-];
+const HistorialCompras = () => {
 
-const rows = [
-  { id: 1, fechaCompra: '2023-06-01', nombrePack: 'Pack 1', metodoPago: 'Tarjeta de Crédito', monto: '50.00' },
-  { id: 2, fechaCompra: '2023-06-02', nombrePack: 'Pack 2', metodoPago: 'Paypal', monto: '75.00' },
-  { id: 3, fechaCompra: '2023-06-03', nombrePack: 'Pack 3', metodoPago: 'Transferencia Bancaria', monto: '100.00' },
-];
-
-export default function HistorialDeCompras() {
+  useTokenValidation();
+  
   return (
     <DashboardLayout>
-      <Box sx={{ textAlign: 'center', mb: 2 }}>
-        <Typography variant="h4" >
-          HISTORIAL DE COMPRAS DE PACKS
-        </Typography>
-      </Box>
-      <Box sx={{ height: 520, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-          checkboxSelection
-          disableRowSelectionOnClick
-        />
-      </Box>
+      <Typography sx={{ 
+        fontSize: "22px", 
+        textAlign: "center", 
+        textTransform: "uppercase", 
+        fontWeight: "800",
+        my: "20px" }}>
+          Historial de Turnos
+      </Typography>
+      <div style={{ margin: "2rem" }}>      
+        <TableBooking />
+      </div>
     </DashboardLayout>
-  );
+  )
 }
+
+export default HistorialCompras
